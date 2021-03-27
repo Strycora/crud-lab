d14b960e50c9bf2660c294368730594cf18decf1
Vikram Chatterjee <strycora@gmail.com>
2021-03-27 16:47:27 -0600

    commit tree e1c1b58769fde0505863d78d2b7fe49d0f4d9b44

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      text: ''
+    }
+  }
+
+  handleOnChange(event){
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleOnSubmit(event){
+    event.preventDefault()
+    this.props.addRestaurant(this.state.text)
+    this.setState({
+      text: ''
+    })
+  }
+
+  
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <input type="text" name="restaurantInput" value={this.state.text} onChange={(event) => this.handleOnChange(event)}></input>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
